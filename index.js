9bbad09940ec7f3ed0c6403e48873fb1ced82001
dylanthelion <dillion256@gmail.com>
2016-07-29 23:49:33 -0500

    postForm Successfully upploaded form content to S3 bucket

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+var AWS = require('aws-sdk');
+var s3 = require('s3');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+var fs = require('fs');
+
+var client = s3.createClient({
+  maxAsyncS3: 20,
+  s3RetryCount: 3, 
+  s3RetryDelay: 1000,
+  multipartUploadThreshold: 20971520,
+  multipartUploadSize: 15728640, 
+  s3Options: {
+    accessKeyId: "",
+    secretAccessKey: "",
+  },
+});
 
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html");
+})
+
+app.post('/sendScholarshipForm', function(req, res) {
+	console.log(req.body.message);
+	var filePath = "public/applications/";
+	var fullPath = filePath + req.body.filename;
+	fs.writeFile(fullPath, req.body.message, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	    console.log("The file was saved!");
+	    var params = {
+		  localFile: fullPath,
+		 
+		  s3Params: {
+		    Bucket: "craig-belcher-scholarship",
+		    Key: req.body.filename,
+		  },
+		};
+
+		var uploader = client.uploadFile(params);
+			uploader.on('error', function(err) {
+			  console.error("unable to upload:", err.stack);
+			});
+			uploader.on('progress', function() {
+			  console.log("progress", uploader.progressMd5Amount,
+			            uploader.progressAmount, uploader.progressTotal);
+			});
+			uploader.on('end', function() {
+			  console.log("done uploading");
+			});
+		}); 
 })
 
 var server = app.listen(8081, function () {
---
+++ public/scripts/form-handler.js
@@ -0,0 +1,63 @@
+$(document).ready(function(event) {
+	console.log('Form working');
+	$('#btnContactUs').click(function(event) {
+		event.preventDefault();
+		var isValid = true;
+		// Validation
+		if (isValid) {
+			writeInput();
+		}
+	});
+});
+
+function writeInput() {
+
+	var outputString = "";
+	var date = Date.now();
+	var file = $('#name').val();
+	file += "_scholarship_";
+	file += date.toString();
+	file += ".txt";
+	//console.log("write input");
+	$('input').each(function() {
+		console.log("One");
+		outputString += $(this).attr('name');
+		outputString += ": ";
+		outputString += $(this).val();
+		outputString += "\n";
+	});
+
+	$('select').each(function() {
+		console.log("One");
+		outputString += $(this).attr('name');
+		outputString += ": ";
+		outputString += $(this).val();
+		outputString += "\n";
+	});
+
+	$('textarea').each(function() {
+		console.log("One");
+		outputString += $(this).attr('name');
+		outputString += ": ";
+		outputString += $(this).val();
+		outputString += "\n";
+	});
+
+
+	$.ajax({
+	  url : '/sendScholarshipForm/',
+	  type: 'POST',
+	  dataType : "json",
+	  data: {
+	  	message: outputString,
+	  	filename: file
+	  },
+	  success: function(data) {
+         alert('success');
+      },
+      error: function() {
+         alert('error');
+      }    
+	});
+	console.log(outputSting);
+}
