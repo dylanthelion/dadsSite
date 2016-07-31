00a689ee94a0b1cdbe01c39f9abe00024d464cc0
dylanthelion <dillion256@gmail.com>
2016-07-30 20:19:31 -0500

    emailAlert John will now be notified with an email after an upload. Credentials were moved to serverVars.json File to be uploaded is temporarily created, then deleted. My email will be used, during the test phase

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 var fs = require('fs');
 var env = require('./serverVars.json');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service:'Gmail',
+    auth: {
+        user : env.Email_Source,
+        pass : env.Password_Email_Source
+    }
+});
 
 var client = s3.createClient({
   maxAsyncS3: 20,
@@ -28,6 +37,7 @@ app.post('/sendScholarshipForm', function(req, res) {
 	console.log(req.body.message);
 	var filePath = "public/applications/";
 	var fullPath = filePath + req.body.filename;
+	var emailtext = "File name: " + filePath + "\nLink: " + env.IAM_URL;
 	fs.writeFile(fullPath, req.body.message, function(err) {
 	    if(err) {
 	        return console.log(err);
@@ -38,24 +48,48 @@ app.post('/sendScholarshipForm', function(req, res) {
 		  localFile: fullPath,
 		 
 		  s3Params: {
-		    Bucket: "craig-belcher-scholarship",
+		    Bucket: env.S3_bucket,
 		    Key: req.body.filename,
 		  },
 		};
 
 		var uploader = client.uploadFile(params);
-			uploader.on('error', function(err) {
-			  console.error("unable to upload:", err.stack);
-			});
-			uploader.on('progress', function() {
-			  console.log("progress", uploader.progressMd5Amount,
+		uploader.on('error', function(err) {
+			console.error("unable to upload:", err.stack);
+		});
+		uploader.on('progress', function() {
+			console.log("progress", uploader.progressMd5Amount,
 			            uploader.progressAmount, uploader.progressTotal);
+		});
+		uploader.on('end', function() {
+			console.log("done uploading");
+			var emailSource = '"Applicant" <' + env.Email_Source + '>';
+			var mailOptions = {
+				from: emailSource,
+				to: env.Email_John,
+				subject: 'New Applicant',
+				text: emailtext
+			};
+			transporter.sendMail(mailOptions, function(error, info){
+				console.log("Sending");
+				if(error){
+				    return console.log(error);
+				}
+				console.log('Message sent: ' + info.response);
+				deleteFile(fullPath);
 			});
-			uploader.on('end', function() {
-			  console.log("done uploading");
-			});
-		}); 
+		});
+	});
 })
+
+function deleteFile(fullPath) {
+	fs.unlink(fullPath, function(err) {
+	   if (err) {
+	       return console.error(err);
+	   }
+	   console.log("File deleted successfully!");
+	});
+}
 
 var server = app.listen(8081, function () {
 
